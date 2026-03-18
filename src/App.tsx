2677846011a2439f86ddd76b9aeecccec73d93b4
contaparaestudo-uwu <contato.anpf@gmail.com>
2026-03-18 14:05:56 -0300

    Alterar global.css para style.ts e exportar para App()

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal from 'styles'
 
 function App() {
   return (
     <>
+      <EstiloGlobal>
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
+      <EstiloGlobal />
     </>
   )
 }
 
-export default App
+export default App;
